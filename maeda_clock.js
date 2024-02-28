43eb50aae07ac6c51b40c72ce7b57f004e36ac80
TaliaHope <161650309+TaliaHope@users.noreply.github.com>
2024-02-29 11:20:24 +1300

    Update maeda_clock.js

--- maeda_clock.js
+++ maeda_clock.js
@@ -1,9 +1,22 @@
 // Update this function to draw you own maeda clock on a 960x500 canvas
 function draw_clock(obj) {
   // YOUR MAIN CLOCK CODE GOES HERE
-  background(50); //  beige
-  fill(200); // dark grey
-  textSize(40);
+  angelMode DEGREES ; 
+  background(200); //  pink
+  fill(0); // dark grey
+  textSize(400);
   textAlign(CENTER, CENTER);
-  text("YOUR MAEDA CLOCK CODE GOES HERE", width/2, height/2);
+  
+  translate (width/2, height/2)
+  Push()
+  rotate ((360/12)+11)
+  Text ("11",0,0)
+  Pop()
+
+  let blockSize = 50;
+
+  noStroke()
+  push()
+  rotate
+  
 }
