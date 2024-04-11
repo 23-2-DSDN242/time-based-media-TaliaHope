b2c5a73e9ef147d08ab74d736b528f217c1f2ec8
TaliaHope <161650309+TaliaHope@users.noreply.github.com>
2024-04-11 20:12:39 +1200

    Update clock.js

--- clock.js
+++ clock.js
@@ -568,6 +568,696 @@ function draw_clock() {
   fill(140, 82, 255); // dark purple
   ellipse(0, 0, 15, 15);
   pop();    
+
+  // background flowers
+  push();
+  translate(450, 62);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();  
+  
+   // background flowers
+   push();
+   translate(420, 110);
+   fill(134, 156, 255); // purple
+   noStroke();
+   for (let i = 0; i < 10; i ++) {
+     ellipse(0, 10, 10, 30);
+     rotate(PI/5);
+   }
+   fill(140, 82, 255); // dark purple
+   ellipse(0, 0, 15, 15);
+   pop(); 
+
+  // background flowers
+  push();
+  translate(490, 100);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();
+  
+  // background flowers
+  push();
+  translate(505, 50);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();     
+
+  // background flowers
+  push();
+  translate(545, 115);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();  
+  
+  // background flowers
+  push();
+  translate(560, 67);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();  
+  
+  // background flowers
+  push();
+  translate(614, 58);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();
+  
+  // background flowers
+  push();
+  translate(600, 108);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();    
+  
+  // background flowers
+  push();
+  translate(590, 165);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();  
+  
+  // background flowers
+  push();
+  translate(640, 147);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+  
+  // background flowers
+  push();
+  translate(650, 90);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();     
+
+  // background flowers
+  push();
+  translate(675, 46);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+  
+  // background flowers
+  push();
+  translate(690, 119);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();     
+
+  // background flowers
+  push();
+  translate(719, 67);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();  
+  
+  // background flowers
+  push();
+  translate(740, 107);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();  
+  
+  // background flowers
+  push();
+  translate(770, 50);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();  
+  
+  // background flowers
+  push();
+  translate(790, 95);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();     
+
+  // background flowers
+  push();
+  translate(823, 60);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+  
+  // background flowers
+  push();
+  translate(873, 50);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();     
+
+  // background flowers
+  push();
+  translate(915, 70);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();     
+
+  // background flowers
+  push();
+  translate(910, 123);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();     
+
+  // background flowers
+  push();
+  translate(860, 96);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();    
+  
+  // background flowers
+  push();
+  translate(822, 125);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();     
+
+  // background flowers
+  push();
+  translate(869, 151);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+  
+  // background flowers
+  push();
+  translate(908, 184);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();    
+  
+  // background flowers
+  push();
+  translate(905, 235);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+
+  // background flowers
+  push();
+  translate(913, 285);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+
+  // background flowers
+  push();
+  translate(875, 325);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+
+  // background flowers
+  push();
+  translate(910, 360);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop(); 
+
+  // background flowers
+  push();
+  translate(890, 400);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+
+  // background flowers
+  push();
+  translate(850, 434);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+
+  // background flowers
+  push();
+  translate(905, 454);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+
+  // background flowers
+  push();
+  translate(840, 381);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+
+  // background flowers
+  push();
+  translate(800, 445);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+
+  // background flowers
+  push();
+  translate(788, 395);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop(); 
+  
+  // background flowers
+  push();
+  translate(746, 425);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+
+  // background flowers
+  push();
+  translate(700, 453);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();  
+  
+  // background flowers
+  push();
+  translate(710, 385);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+
+  // background flowers
+  push();
+  translate(660, 420);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+
+  // background flowers
+  push();
+  translate( 655, 359);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+
+  // background flowers
+  push();
+  translate(600, 340);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop(); 
+  
+  // background flowers
+  push();
+  translate(613, 395);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+
+  // background flowers
+  push();
+  translate(625, 455);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop(); 
+  
+  // background flowers
+  push();
+  translate(580, 436);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+
+  // background flowers
+  push();
+  translate(555, 382);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();  
+  
+  // background flowers
+  push();
+  translate(530, 450);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+  
+  // background flowers
+  push();
+  translate(507, 400);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();    
+
+  // background flowers
+  push();
+  translate(480, 442);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();  
+  
+  // background flowers
+  push();
+  translate(432, 454);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();    
+
+  // background flowers
+  push();
+  translate(445, 400);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();    
+
+  // background flowers
+  push();
+  translate(400, 380);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();   
+  
+  // background flowers
+  push();
+  translate(353, 355);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();    
+
+  // background flowers
+  push();
+  translate(367, 430);
+  fill(134, 156, 255); // purple
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 10, 10, 30);
+    rotate(PI/5);
+  }
+  fill(140, 82, 255); // dark purple
+  ellipse(0, 0, 15, 15);
+  pop();    
 }
+
 
 
